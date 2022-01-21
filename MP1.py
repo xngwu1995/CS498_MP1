bafa46a18c8d99e3047fb1d8db678583fbc88cba
xngwu1995 <xngwu1995@gmail.com>
2022-01-21 13:16:55 -0600

    Finished

--- MP1.py
+++ MP1.py
@@ -2,6 +2,7 @@ import random
 import os
 import string
 import sys
+import re
 
 stopWordsList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
             "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
@@ -13,7 +14,7 @@ stopWordsList = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "y
             "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
             "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
             "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
-
+stopWordsList = set(stopWordsList)
 delimiters = " \t,;.?!-:@[](){}_*/"
 
 def getIndexes(seed):
@@ -29,8 +30,22 @@ def process(userID):
     indexes = getIndexes(userID)
     ret = []
     # TODO
-                    
-    for word in ret:
+    word_dict = {}
+    content = sys.stdin.readlines()
+    for idx in indexes:
+        # clean and count word
+        line = content[idx]
+        add_word(word_dict, line)
+    sorted_word_dict = sorted(word_dict.items(), key = lambda item: (-item[1], item[0]))
+    for word, freq in sorted_word_dict[:20]:
         print(word)
 
+def add_word(word_dict, line):
+    line_split = re.split(r'[ \t,;.?!\-:@[\\\](){}_*/]', line)
+    for word in line_split:
+        word = word.strip().lower()
+        if word not in stopWordsList and word:
+            word_dict[word] = word_dict.get(word, 0) + 1
+    
+    
 process(sys.argv[1])
